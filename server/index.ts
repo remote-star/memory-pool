7e320e70419d1e0a1d0d4bf9c452760c4294ea96
remote-star <148054685@qq.com>
2018-10-11 20:54:14 +0800

    auth during upload

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ import * as Router from 'koa-router'
 import * as BodyParser from 'koa-bodyparser'
 import * as mongoose from 'mongoose'
 
+import * as crypto from 'crypto'
+
 const app = new Koa()
 const router = new Router()
 
@@ -26,11 +28,29 @@ const BlogPost = new Schema({
 const PostModel = mongoose.model('post', BlogPost)
 
 router.post('/api/post', (ctx, next) => {
-  const instance = new PostModel() as any
+  if (!ctx.request.body) {
+    ctx.status = 400
+    return
+  }
+
   const body = ctx.request.body as {
+    pwd: string
     title: string
     content: string
   }
+
+  const md5 = crypto.createHash('md5')
+  const salt = 'thisIsReallyALofOfSalt'
+  md5.update(body.pwd + salt)
+  const md5Str = md5.digest('hex')
+
+  if ('25333dfb17252abfb7bd77f91e0fade7' !== md5Str) {
+    ctx.status = 401
+    ctx.body = '别瞎传'
+    return
+  }
+
+  const instance = new PostModel() as any
 
   instance.title = body.title
   instance.content = body.content
